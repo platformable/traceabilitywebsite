e595f66d3e365417c525f6939c9aa0ddae05761c
leoncan122 <leon.can122@gmail.com>
2024-08-28 16:56:15 +0200

    Update Layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Hero() {
  
   return (
-    <section className='text-center text-[#3423C5]'>
+    <section className='pt-10 container mx-auto text-center text-[#3423C5]'>
       <h1 className='font-bold text-lg mb-4'>Open Digital Ecosystems</h1>
-      <h2 className='font-bold text-4xl mb-10'>How Value is generated and distributed amongst stakeholders in a sustainability/traceability data ecosystem</h2>
+      <h2 className='font-bold text-4xl mb-20'>How Value is generated and distributed amongst stakeholders in a sustainability/traceability data ecosystem</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 md:gap-8 mb-12'>
         <div className='grid grid-rows-[1fr_4fr] font-medium'>
           <div className='h-1 rounded bg-[#3423C5]'></div>
