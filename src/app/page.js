7e5335e099f1e889911aef06a03b246163833eb9
leoncan122 <leon.can122@gmail.com>
2024-12-17 11:44:58 +0100

    chore: Improve home, Add access control

--- src/app/page.js
+++ src/app/page.js
@@ -4,21 +4,21 @@ import { redirect } from "next/navigation";
 
 
 export default async function Home() {
-  const session = await getSession();
+  // const session = await getSession();
   // console.log("session /", session)
-  if (session) {
-    return redirect('/admin-panel')
-  }
+  // if (session) {
+  //   return redirect('/admin-panel')
+  // }
   return (
     <main id="home-content" className="bg-primary-gradient min-h-screen max-h-screen overflow-hidden  py-20">
       <div className="max-w-screen-xl mx-auto grid lg:flex px-5 lg:p-0">
       <section className=" bg-white p-14 flex flex-col gap-7 lg:w-[35%] items-center lg:items-start">
         <div>
-            <img src="/open_health_dashboard_logo_home.svg" alt="health dashboard logo" />
+            <img src="/traceability-logo-home.svg" alt="Traceability logo" />
         </div>
         <div id="hero-description" className="">
-          <div className="text-[var(--home-highlight)] text-[46px] leading-snug">
-            <span className="">Hello,</span> <br/>
+          <div className=" text-[46px] leading-snug">
+            <span className="text-[var(--highlight-pink)]">Hello,</span> <br/>
            <strong className="text-black"> Welcome!</strong>
           </div>
         </div>
@@ -26,12 +26,12 @@ export default async function Home() {
         Get updates on global open health regulations, understand digital health strategies to boost healthcare access, drive data-driven decisions, improve efficiency, and promote equity for impactful outcomes and cost savings.
         </p>
         <Link id="login-link" href="/api/auth/login">
-        <button className="bg-[var(--home-highlight)] px-7 text-white font-medium rounded-md shadow py-2">Login / Register</button>
+        <button className="bg-[var(--highlight-pink)] px-7 text-white font-medium rounded-md shadow py-2">Login / Register</button>
         </Link>
         
       </section>
-      <div id="hero-home-img" className="hidden lg:block lg:w-[65%] p-10 hero-home-img">
-        <img src="/welcome-page.png" alt="Open Health dashboard features image" className="object-fill"/>
+      <div id="hero-home-img" className="hidden lg:flex items-center justify-center p-10 hero-home-img">
+        <img src="/welcome-page.png" alt="Traceability dashboard features image" className=""/>
         
       </div>
       </div>
