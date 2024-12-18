5e36a3e7276e4ac86553ecc062f67aea44999997
Alexei Garban <garban.valdeon@gmail.com>
2024-12-18 19:49:37 +0100

    suppor form

--- src/app/lib/nocodb-traceability-table.js
+++ src/app/lib/nocodb-traceability-table.js
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 'use server'
 import { Api } from 'nocodb-sdk'
-
+import { NextResponse } from 'next/server'
+import { redirect } from "next/navigation";
 
 export async function getEcosystemTraceabilityTableData(selectedOption) {
 /*  console.log("executing getEcosystemTraceabilityTableData",prevState) */
@@ -27,7 +28,7 @@ export async function getEcosystemTraceabilityTableData(selectedOption) {
     }
     const tableSelected = selectTable()
 /*     console.log("table selected",tableSelected) */
-    const data = await api.dbViewRow.list(
+    const data = await api.dbViewRow.pos(
         "noco",
         "[DASHBOARD] - Open traceablity",
         tableSelected,
@@ -45,3 +46,53 @@ export async function getEcosystemTraceabilityTableData(selectedOption) {
   }
 }
 
+
+export async function sendSupportRequest(clientToken,formData) {
+  
+
+  const data = {
+    "Application": 'traceability',
+    "UserId": formData.get("user_id").trim(),
+    "Email": formData.get("email").trim(),
+    "Subject": formData.get("subject").trim(),
+    "Description": formData.get("message").trim(),
+
+  };
+
+  try {
+    fetch(
+      "https://nocodb-app-agy4g.ondigitalocean.app/api/v1/db/data/noco/Platformable Operational/SupportRequest",
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "xc-auth": process.env.NEXT_PUBLIC_NOCODB_AUTH_TOKEN
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => {console.log("support message sent")
+
+         
+    
+      }
+      
+    )  // Parse the response as JSON
+      .then(data => {
+        // Handle successful response with data
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+        // Handle the error, like displaying an error message to the user
+      });
+
+  } catch (error) {
+    console.log("PASA NO OK")
+    //redirect(`/admin-panel`);
+    console.log("support request error", error)
+    return {
+      message: "Something went wrong, try again",
+      error: true
+    };
+  }
+  return { error: null, success: true }
+}
---
+++ src/components/support-request/Form.js
@@ -0,0 +1,132 @@
+"use client";
+
+import {  useEffect, useRef, useState, useTransition } from "react";
+import { sendSupportRequest } from "@/app/lib/nocodb-traceability-table";
+import { useFormState } from "react-dom";
+import Loader from "../Loader";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
+export default function Form({userId, email,clientToken}) {
+    const formRef = useRef(null)
+  const initialState = { message: null, errors: {} };
+  const router = useRouter()
+  const [state, formAction] = useFormState(sendSupportRequest, initialState);
+  const [isLoading, setIsLoading] = useState(false)
+  const [isPending, startTransition] = useTransition();
+
+  
+
+  useEffect(() => {
+    let timeOutID;
+    setIsLoading(true)
+
+    if (!state.success) {
+     
+        toast.error(state.message);
+     } else {
+        toast.success(state.message);
+        timeOutID = setTimeout(() =>   router.push('/admin-panel'),2000)
+      }
+  
+    setIsLoading(false)
+
+    return () => clearTimeout(timeOutID);
+  }, [state]);
+  
+  /* const updateUserWithClientToken = sendSupportRequest.bind(null, clientToken) */
+
+  console.log("state",state)
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    
+      <form
+        action={formAction}
+        className="grid gap-y-6  mt-10 "
+        ref={formRef}
+      >
+         <input
+          type="text"
+          defaultValue={userId}
+          name="user_id"
+          hidden
+          readOnly
+        />
+        <input
+          type="text"
+          defaultValue={clientToken}
+          name="clientToken"
+          hidden
+          readOnly
+        />
+        <div>
+          <label className="uppercase font-bold text-xs mb-2 block">Email</label>
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            // disabled
+            className="pointer-events-none py-2 px-3 border rounded border-black w-full"
+            required
+          />
+        </div>
+        <div>
+          <label className="uppercase font-bold text-xs mb-2 block">Subject</label>
+          <select name="subject" className=" py-2 px-3 border rounded border-black w-full" required>
+            <option value="" className={``}></option>
+            <option value="Bug issue/site not working properly" className={``}>
+            Bug issue/site not working properly
+            </option>
+            <option value="Page doesn’t load" className={``}>
+            Page doesn’t load
+            </option>
+            <option value="Can’t download file" className={``}>
+            Can’t download file
+            </option>
+            <option value="Feature request idea" className={``}>
+            Feature request idea
+            </option>
+            <option value="Other" className={``}>
+            Other
+            </option>
+          </select>
+        </div>
+
+        <div>
+          <label className="uppercase font-bold text-xs mb-2 block">Message</label>
+
+          <textarea
+            name="message"
+            required
+            className="w-full h-48 border border-black rounded px-3 py-2"
+          />
+        </div>
+        {!isPending ? (
+        <center>
+              <button
+           //type="submit"
+            onClick={() => startTransition(() => formRef.current.requestSubmit())}
+            className="bg-[var(--button-background-forms)] font-bold py-2 px-6 rounded shadow "
+          >
+            SEND
+          </button>
+        </center>
+        ) : (
+          <Loader />
+        )}
+      </form>
+    </>
+  );
+}
