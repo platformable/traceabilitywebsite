84ec6fedff358028483f100af479c7c57115542e
leoncan122 <leon.can122@gmail.com>
2024-12-18 16:46:53 +0100

    Add diagram legend & fix footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-5 w-full text-center pb-4">
+    <footer className="w-full text-center pb-8">
         By{" "}
         <img src="/platformable-logo-black.svg" alt="icon" className="inline" width={160}/>
     </footer>
